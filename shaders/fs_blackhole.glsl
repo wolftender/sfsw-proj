9ea0eec8a1fb36b0127959cbb0b2cfddfa44d003
wolftender <etheryt@protonmail.com>
2024-06-09 20:10:09 +0200

    add numeric integration of light bending equation

--- shaders/fs_blackhole.glsl
+++ shaders/fs_blackhole.glsl
@@ -24,6 +24,61 @@ vec3 rodrigues(vec3 v, vec3 k, float theta) {
     return v*c + cross(k,v)*s + k*dot(k,v)*(1.0 - c);
 }
 
+float ff(float b, float M, float w) {
+    return 1.0 - w*w*(1.0 - 2.0*M*w/b);
+}
+
+float f(float b, float M, float w) {
+    return sqrt(max(0.0, 1.0 - w*w*(1.0 - 2.0*M*w/b)));
+}
+
+float find_w1(float b, float M) {
+    float wl = 0.0;
+    float wr = 2.0;
+    float wm = 1.0;
+
+    for (int i = 0; i < 500; ++i) {
+        wm = 0.5 * (wl + wr);
+
+        float hl = ff(b, M, wl);
+        float hm = ff(b, M, wm);
+        float hr = ff(b, M, wr);
+
+        if (hm > 0.0) {
+            wl = wm;
+        } else {
+            wr = wm;
+        }
+    }
+
+    return wl;
+}
+
+float find_angle(float b, float M) {
+    float limit = find_w1(b,M);
+    int steps = 2000;
+    float dw = limit / float(steps);
+
+    float wl = 0.0;
+    float F = 0.0;
+
+    for (int i = 0; i < steps; ++i) {
+        float wr = dw * float(i);
+        float hl = f(b, M, wl);
+        float hr = f(b, M, wr);
+
+        hl = 1.0/hl;
+        hr = 1.0/hr;
+
+        float dF = 0.5f * dw * (hl + hr);
+
+        F += dF;
+        wl = wr;
+    }
+
+    return F;
+}
+
 // assume that the black hole is in (0,0,0)
 // assume that the camera distance is u_distance
 // assume that the black hole M = u_star_mass
@@ -55,8 +110,8 @@ void main() {
     float Msq = M * M;
 
     if (1.0/bsq < 1.0/(Msq * 27.0)) {
-        // TODO: replace with real integral
-        float dtheta = (1.0 / (1.0 / sqrt(27.0) - M / b) - sqrt(27.0)) / 12.0;
+        //float dtheta = (1.0 / (1.0 / sqrt(27.0) - M / b) - sqrt(27.0)) / 12.0;
+        float dtheta = 2.0 * find_angle(b, M);
 
         vec3 left = normalize(p0 - star_worldpos);
         vec3 forward = normalize(star_worldpos - cam_worldpos);
